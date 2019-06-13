f49ad4e4b31b2276c1abb4b023cc4f5e838a43a3
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-06-12 22:21:27 -0700

    moves to numpy slicing, nearly as fast and no edge effects

--- Sandpile Main.py
+++ Sandpile Main.py
@@ -4,14 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-
-
 def topple(grid):
     mask = grid > 3
-    grid += np.roll(mask, 1, axis=1)
-    grid += np.roll(mask, -1, axis=1)
-    grid += np.roll(mask, 1, axis=0)
-    grid += np.roll(mask, -1, axis=0)
+    grid[:,1:] += mask[:,:-1]
+    grid[:,:-1] += mask[:,1:]
+    grid[1:,:] += mask[:-1,:]
+    grid[:-1,:] += mask[1:,:]
     grid -= mask*4
 
 def draw_graph(grid, n):
@@ -26,22 +24,17 @@ def draw_graph(grid, n):
     plt.show()
 
 def run(n, grid_size):
-    topple_counter = 0
-
     grid = np.zeros([grid_size, grid_size])
     center = grid_size//2
     grid[center, center] = n
 
     while grid[center,center] > 3 or grid.max() > 3:
         topple(grid)
-        if grid[center,0] != 0:
-            print("canvas is too small!!")
-            break
     return grid
 
 def main():
     n = 50000
-    grid_size = 200
+    grid_size = 100
 
     if grid_size % 2 ==0:
         grid_size -= 1
@@ -51,6 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
