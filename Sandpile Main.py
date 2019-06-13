46eb2df0b022d3fa802b03d658a0079ee95723af
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-06-12 21:39:51 -0700

    make code 0.1 times faster by prechecking the center pixel

--- Sandpile Main.py
+++ Sandpile Main.py
@@ -19,13 +19,6 @@ def topple(grid):
     grid += np.roll(mask, -1, axis=0)
     grid -= mask*4
 
-def unstable(grid):
-    unstable_list = []
-    for (x,y), i in np.ndenumerate(grid):
-        if i > 3:
-            unstable_list.append((x,y))
-    return unstable_list
-
 def draw_graph(grid):
     cmap = colors.ListedColormap(['black','yellow','orange','red'])
     bounds = [-.5, .5, 1.5, 2.5, 3.5]
@@ -42,11 +35,11 @@ def run(n):
     grid = np.zeros([grid_size, grid_size])
     grid[center, center] = n
 
-    while grid.max() > 3:
+    while grid[center,center] > 3 or grid.max() > 3:
         topple(grid)
     return grid
 
 if __name__ == "__main__":
     from timeit import timeit
     print(timeit("run(300)", number=3, setup="from __main__ import run"))
-    # 2.6293157510001492 before;  after 0.10332873700099299
+    # before 0.10332873700099299; after 0.09848564000094484
