c995c69c814c889d7c9e04375d8845b812ea403f
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-06-12 19:23:05 -0700

    move functions out of nests

--- Sandpile Main.py
+++ Sandpile Main.py
@@ -9,47 +9,34 @@ from matplotlib import colors
 grid_size = 99
 if grid_size % 2 ==0:
     grid_size -= 1
-center = math.floor(grid_size/2)
+center = grid_size//2
 
+def topple(grid, i, j):
+    grid[i, j + 1] += 1
+    grid[i, j - 1] += 1
+    grid[i + 1, j] += 1
+    grid[i - 1, j] += 1
+    grid[i, j] -= 4
 
-def run(n):
-    topple_counter = 0
+def unstable(grid):
+    unstable_list = []
+    for (x,y), i in np.ndenumerate(grid):
+        if i > 3:
+            unstable_list.append((x,y))
+    return unstable_list
 
-    grid = np.zeros([grid_size, grid_size])
+def stable(grid):
+    counter = 0
+    for (x, y), i in np.ndenumerate(grid):
+        if i > 3:
+            counter += 1
+    if counter == 0:
+        print('Stable')
+        return True
+    else:
+        return False
 
-    def topple(i, j):
-        grid[i, j + 1] += 1
-        grid[i, j - 1] += 1
-        grid[i + 1, j] += 1
-        grid[i - 1, j] += 1
-        grid[i, j] -= 4
-
-    def unstable():
-        unstable_list = []
-        for (x,y), i in np.ndenumerate(grid):
-            if i > 3:
-                unstable_list.append((x,y))
-        return unstable_list
-
-    def stable():
-        counter = 0
-        for (x, y), i in np.ndenumerate(grid):
-            if i > 3:
-                counter += 1
-        if counter == 0:
-            print('Stable')
-            return True
-        else:
-            return False
-
-    grid[center, center] = n
-
-    while not stable():
-        to_topple = unstable()
-        for (i, j) in to_topple:
-            topple(i, j)
-
-
+def draw_graph(grid):
     cmap = colors.ListedColormap(['black','yellow','orange','red'])
     bounds = [-.5, .5, 1.5, 2.5, 3.5]
     norm = colors.BoundaryNorm(bounds, cmap.N)
@@ -59,6 +46,20 @@ def run(n):
     plt.colorbar(heatmap, ticks=[0, 1, 2, 3])
     plt.show()
 
+def run(n):
+    topple_counter = 0
+
+    grid = np.zeros([grid_size, grid_size])
+    grid[center, center] = n
+
+    while not stable(grid):
+        to_topple = unstable(grid)
+        for (i, j) in to_topple:
+            topple(grid, i, j)
+
+    return grid
+
 
 if __name__ == "__main__":
-    run(300)
+    grid = run(300)
+    draw_graph(grid)
