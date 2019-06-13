280e34e9fdf08f4810499d6ee49722d946da2325
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-06-12 19:29:58 -0700

    2x faster by removing the stable function

--- Sandpile Main.py
+++ Sandpile Main.py
@@ -25,17 +25,6 @@ def unstable(grid):
             unstable_list.append((x,y))
     return unstable_list
 
-def stable(grid):
-    counter = 0
-    for (x, y), i in np.ndenumerate(grid):
-        if i > 3:
-            counter += 1
-    if counter == 0:
-        print('Stable')
-        return True
-    else:
-        return False
-
 def draw_graph(grid):
     cmap = colors.ListedColormap(['black','yellow','orange','red'])
     bounds = [-.5, .5, 1.5, 2.5, 3.5]
@@ -52,14 +41,17 @@ def run(n):
     grid = np.zeros([grid_size, grid_size])
     grid[center, center] = n
 
-    while not stable(grid):
-        to_topple = unstable(grid)
+    to_topple = [(center, center)]
+    while to_topple:
         for (i, j) in to_topple:
             topple(grid, i, j)
+        to_topple = unstable(grid)
 
     return grid
 
-
 if __name__ == "__main__":
-    grid = run(300)
-    draw_graph(grid)
+    # ~ grid = run(300)
+    # ~ draw_graph(grid)
+    from timeit import timeit
+    print(timeit("run(300)", number=3, setup="from __main__ import run"))
+    # 4.965258703000018 before; 2.6293157510001492 after
