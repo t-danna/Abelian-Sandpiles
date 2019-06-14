ccaceac5d7f5047ac3ce0a449ba4aab85920106a
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-06-14 10:12:22 -0700

    adds animation

--- Sandpile Main.py
+++ Sandpile Main.py
@@ -2,55 +2,49 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from matplotlib import colors
-
-
 
 def topple(grid):
     mask = grid > 3
-    grid += np.roll(mask, 1, axis=1)
-    grid += np.roll(mask, -1, axis=1)
-    grid += np.roll(mask, 1, axis=0)
-    grid += np.roll(mask, -1, axis=0)
+    grid[:,1:] += mask[:,:-1]
+    grid[:,:-1] += mask[:,1:]
+    grid[1:,:] += mask[:-1,:]
+    grid[:-1,:] += mask[1:,:]
     grid -= mask*4
 
-def draw_graph(grid, n):
-    cmap = colors.ListedColormap(['black','yellow','orange','red'])
-    bounds = [-.5, .5, 1.5, 2.5, 3.5]
-    norm = colors.BoundaryNorm(bounds, cmap.N)
-    heatmap = plt.pcolor(grid, cmap=cmap, norm=norm)
-
-    plt.imshow(grid)
-    plt.colorbar(heatmap, ticks=[0, 1, 2, 3])
-    plt.title(f"Abelian Sandpile with {n:,} starting grains")
-    plt.show()
-
-def run(n, grid_size):
-    topple_counter = 0
-
+def draw_graph(grid_size, n):
     grid = np.zeros([grid_size, grid_size])
     center = grid_size//2
     grid[center, center] = n
+    
+    fig = plt.figure()
+    cmap = colors.ListedColormap(['black','yellow','orange','red'])
+    im = plt.imshow(grid, animated=True, cmap=cmap, vmax=3)
+    bounds = [-.5, .5, 1.5, 2.5, 3.5]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    heatmap = plt.pcolor(grid, cmap=cmap, norm=norm)
+    plt.colorbar(heatmap, ticks=[0, 1, 2, 3])
+    plt.title(f"Abelian Sandpile with {n:,} starting grains")
 
-    while grid[center,center] > 3 or grid.max() > 3:
+    def ani_update(val):
+        if grid[center,center] <= 3 and grid.max() <= 3:
+            ani._stop()
         topple(grid)
-        if grid[center,0] != 0:
-            print("canvas is too small!!")
-            break
-    return grid
+        im.set_array(grid)
+        return im,
+
+    ani = animation.FuncAnimation(fig, ani_update, interval=50, blit=True)
+    plt.show()
 
 def main():
-    n = 50000
-    grid_size = 200
+    n = 5000
+    grid_size = 100
 
     if grid_size % 2 ==0:
         grid_size -= 1
 
-    grid = run(n, grid_size)
-    draw_graph(grid, n)
+    draw_graph(grid_size, n)
 
 if __name__ == "__main__":
     main()
-
-
-
